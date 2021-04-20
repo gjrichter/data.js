9d876f40cb3d84c8436dce4e9e9272a5c5158988
gjrichter <guenter.richter@gmail.com>
2021-04-20 22:17:08 +0200

    Update data.js

--- data.js
+++ data.js
@@ -94,6 +94,8 @@ $Log:data.js,v $
 		errors: []
 	};
 
+	var Papa = null;
+	
 	function expose() {
 		var oldData = window.Data;
 
@@ -511,7 +513,7 @@ $Log:data.js,v $
 
 		// GR 02.11.2015 nuovo csv parser Papa Parse by Matt Hold 
 		// GR 21.07.2016 if autodecet delimiter fails, try first ; and then ,   
-		/**
+
 		if ( typeof(Papa) == "undefined" ){
 			__this = this;
 			$.getScript("https://cdnjs.cloudflare.com/ajax/libs/PapaParse/4.1.2/papaparse.min.js")
@@ -525,7 +527,7 @@ $Log:data.js,v $
 			});
 			return;
 		}
-		**/
+		
 		var newData = Papa.parse(csv,opt.parser).data;
 
 		if ( typeof(newData[0]) == "undefined" || 
